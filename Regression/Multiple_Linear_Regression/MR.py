294ff952020336b688317d0e4a995dcf04f1b841
mpereira <merce.pereira.cao@gmail.com>
2019-03-13 20:23:52 +0100

    change deprecated cross_validation to model_selection

--- Regression/Multiple_Linear_Regression/MR.py
+++ Regression/Multiple_Linear_Regression/MR.py
@@ -23,7 +23,7 @@ X=X[:, 1:]
 
 
 
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,random_state=0)
 
 # feature scaling
@@ -39,7 +39,7 @@ regressor=LinearRegression()
 regressor.fit(X_train,Y_train)
 
 
-Y_pred=regressor.predict(X_test)
+y_pred=regressor.predict(X_test)
 
 
 #Building the optimal model using backward Elimination
